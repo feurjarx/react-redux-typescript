c676379b932f0f7abd256c6cacf8ab67268fbde5
Roman <feurjarx@gmail.com>
2017-03-01 21:23:35 +0600

    preparing struct

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import {App} from "./containers/App";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {todoApp} from "./reducers";
-// import AppContainer from "./containers/AppContainer";
 
 const root = document.getElementById('root');
 const store = createStore(todoApp);
@@ -12,11 +11,9 @@ const store = createStore(todoApp);
 function updateRender() {
 
     const jsxContainer = (
-        // <AppContainer>
-            <Provider store={ store }>
-                <App />
-            </Provider>
-        // </AppContainer>
+        <Provider store={ store }>
+            <App />
+        </Provider>
     );
 
     ReactDOM.render(jsxContainer, root);
