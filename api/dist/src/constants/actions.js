1e48e5c80f392a8502b53894cc18ea1946b98496
Roman <feurjarx@gmail.com>
2017-03-12 22:19:34 +0600

    completed finish event

--- api/dist/src/constants/actions.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.UPDATE_MONITOR_ITEM = 'update_monitor_item';
-exports.INITIAL_LIFE_DATA = 'initial_life_data';
--- api/dist/src/constants/events.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.EVENT_IO_LIFE = 'life';
-exports.EVENT_IO_CONNECTION = 'connection';
-exports.EVENT_IO_DISCONNECT = 'disconnect';
