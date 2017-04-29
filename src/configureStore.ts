aaba26f0b0a207d510f180d254f6ae6e5a3f54d2
Roman <feurjarx@gmail.com>
2017-04-30 01:40:54 +0600

    apply middleware redux-logger

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -3,10 +3,12 @@ import * as Redux from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
+import logger from 'redux-logger';
+
 export default function(inititalState = {}) {
     return Redux.createStore(
         rootReducer,
         inititalState,
-        Redux.applyMiddleware(thunk)
+        Redux.applyMiddleware(thunk, logger)
     );
 }
